2479efe58bcba1bfbb5701910c28edad1ace831a
xinz <joy2everyone.z@gmail.com>
2019-12-24 00:06:00 +0800

    Version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SaxMap.MixProject do
   def project do
     [
       app: :sax_map,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
