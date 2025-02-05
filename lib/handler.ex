37d339cc19d4c7e7c8715faa93bd11d14ebf114c
xinz <joy2everyone.z@gmail.com>
2025-02-05 17:30:00 +0800

    Prune SAXMap.Handler

--- lib/handler.ex
+++ lib/handler.ex
@@ -11,212 +11,33 @@ defmodule SAXMap.Handler do
   @key_content "content"
 
   def handle_event(:start_document, _prolog, opts) do
-    {:ok, {[], Map.new(opts)}}
+    ignore_attribute = Keyword.get(opts, :ignore_attribute, true)
+    {:ok, {[], ignore_attribute}}
   end
 
-  def handle_event(
-        :start_element,
-        {tag_name, _attributes},
-        {stack, %{ignore_attribute: true} = options}
-      ) do
-    stack = prepare_stack_text_when_start_element(stack)
-    tag = {tag_name, nil}
-    {:ok, {[tag | stack], options}}
+  def handle_event(:start_element, element, {stack, ignore_attribute}) do
+    stack = handle_start_element(element, stack, ignore_attribute)
+    {:ok, {stack, ignore_attribute}}
   end
 
-  def handle_event(
-        :start_element,
-        {tag_name, attributes},
-        {stack, %{ignore_attribute: false} = options}
-      ) do
-    stack = prepare_stack_text_when_start_element(stack)
-    tag = {tag_name, attributes, nil}
-    {:ok, {[tag | stack], options}}
+  def handle_event(:cdata, cdata, {stack, ignore_attribute}) do
+    stack = handle_cdata(cdata, stack)
+    {:ok, {stack, ignore_attribute}}
   end
 
-  def handle_event(
-        :start_element,
-        {tag_name, attributes},
-        {stack, %{ignore_attribute: {false, attribute_prefix}} = options}
-      ) do
-    stack = prepare_stack_text_when_start_element(stack)
-    attributes =
-      Enum.map(attributes, fn {key, value} ->
-        {"#{attribute_prefix}#{key}", value}
-      end)
-
-    tag = {tag_name, attributes, nil}
-    {:ok, {[tag | stack], options}}
+  def handle_event(:characters, chars, {stack, ignore_attribute}) do
+    stack = handle_characters(chars, stack, simplify_ignore_attribute_opt(ignore_attribute))
+    {:ok, {stack, ignore_attribute}}
   end
 
-  def handle_event(:cdata, cdata, {[{tag_name, _} | stack], options}) do
-    current = {tag_name, cdata}
-    {:ok, {[current | stack], options}}
+  def handle_event(:end_element, _tag_name, {stack, ignore_attribute}) do
+    stack = handle_end_element(stack, simplify_ignore_attribute_opt(ignore_attribute))
+    {:ok, {stack, ignore_attribute}}
   end
 
-  def handle_event(:cdata, cdata, {[{tag_name, attributes, _} | stack], options}) do
-    current = {tag_name, attributes, cdata}
-    {:ok, {[current | stack], options}}
-  end
-
-  def handle_event(:characters, "\r" <> _ = chars, state) do
-    ignore_or_extract_characters(chars, state)
-  end
-
-  def handle_event(:characters, "\n" <> _ = chars, state) do
-    ignore_or_extract_characters(chars, state)
-  end
-
-  def handle_event(
-        :characters,
-        chars,
-        {[{_not_end_tag, prepared}] = _stack, _} = state
-      )
-      when prepared != nil do
-    ignore_or_extract_characters(chars, state)
-  end
-
-  def handle_event(:characters, chars, state) do
-    extract_characters(chars, state)
-  end
-
-  def handle_event(
-        :end_element,
-        tag_name,
-        {[{tag_name, content} | []], %{ignore_attribute: true} = options}
-      ) do
-    current = {tag_name, content}
-    {:ok, {current, options}}
-  end
-
-  def handle_event(
-        :end_element,
-        tag_name,
-        {[{tag_name, attributes, content} | []], %{ignore_attribute: false} = options}
-      ) do
-    current = {tag_name, attributes, content}
-    {:ok, {current, options}}
-  end
-
-  def handle_event(
-        :end_element,
-        tag_name,
-        {[{tag_name, attributes, content} | []],
-         %{ignore_attribute: {false, _attribute_prefix}} = options}
-      ) do
-    current = {tag_name, attributes, content}
-    {:ok, {current, options}}
-  end
-
-  def handle_event(
-        :end_element,
-        tag_name,
-        {[{tag_name, content} | [{parent_tag_name, nil} | rest]],
-         %{ignore_attribute: true} = options}
-      ) do
-    current = {tag_name, format_key_value_pairs(content)}
-    parent = {parent_tag_name, [current]}
-    {:ok, {[parent | rest], options}}
-  end
-
-  def handle_event(
-        :end_element,
-        tag_name,
-        {[{tag_name, attributes, content} | [{parent_tag_name, parent_attributes, nil} | rest]],
-         %{ignore_attribute: false} = options}
-      ) do
-    formated_content = format_key_value_pairs(content)
-    current = %{tag_name => format_key_value_pairs([{@key_content, formated_content} | attributes])}
-    parent = {parent_tag_name, parent_attributes, [current]}
-    {:ok, {[parent | rest], options}}
-  end
-
-  def handle_event(
-        :end_element,
-        tag_name,
-        {[{tag_name, attributes, content} | [{parent_tag_name, parent_attributes, nil} | rest]],
-         %{ignore_attribute: {false, _attribute_prefix}} = options}
-      ) do
-    formated_content = format_key_value_pairs(content)
-    current = %{tag_name => format_key_value_pairs([{@key_content, formated_content} | attributes])}
-    parent = {parent_tag_name, parent_attributes, [current]}
-    {:ok, {[parent | rest], options}}
-  end
-
-  def handle_event(
-        :end_element,
-        tag_name,
-        {[{tag_name, content} | [{parent_tag_name, parent_content} | rest]],
-         %{ignore_attribute: true} = options}
-      ) do
-    current = {tag_name, format_key_value_pairs(content)}
-    parent = {parent_tag_name, [current | parent_content]}
-    {:ok, {[parent | rest], options}}
-  end
-
-  def handle_event(
-        :end_element,
-        tag_name,
-        {[
-           {tag_name, attributes, content}
-           | [{parent_tag_name, parent_attributes, parent_content} | rest]
-         ], %{ignore_attribute: false} = options}
-      ) do
-    formated_content = format_key_value_pairs(content)
-    current = %{tag_name => format_key_value_pairs([{@key_content, formated_content} | attributes])}
-    parent = {parent_tag_name, parent_attributes, [current | parent_content]}
-    {:ok, {[parent | rest], options}}
-  end
-
-  def handle_event(
-        :end_element,
-        tag_name,
-        {[
-           {tag_name, attributes, content}
-           | [{parent_tag_name, parent_attributes, parent_content} | rest]
-         ], %{ignore_attribute: {false, _attribute_prefix}} = options}
-      ) do
-    formated_content = format_key_value_pairs(content)
-    current = %{tag_name => format_key_value_pairs([{@key_content, formated_content} | attributes])}
-    parent = {parent_tag_name, parent_attributes, [current | parent_content]}
-    {:ok, {[parent | rest], options}}
-  end
-
-  def handle_event(:end_document, _, {{key, nil}, %{ignore_attribute: true}}) do
-    {:ok, %{key => %{}}}
-  end
-
-  def handle_event(:end_document, _, {{key, attributes, nil}, %{ignore_attribute: false}}) do
-    {:ok, %{key => format_key_value_pairs(attributes)}}
-  end
-
-  def handle_event(
-        :end_document,
-        _,
-        {{key, attributes, nil}, %{ignore_attribute: {false, _attribute_prefix}}}
-      ) do
-    {:ok, %{key => format_key_value_pairs(attributes)}}
-  end
-
-  def handle_event(:end_document, _, {{key, value}, %{ignore_attribute: true}}) do
-    {:ok, %{key => format_key_value_pairs(value)}}
-  end
-
-  def handle_event(:end_document, _, {{key, attributes, value}, %{ignore_attribute: false}}) do
-    content =
-      format_key_value_pairs(attributes) |> Map.put(@key_content, format_key_value_pairs(value))
-
-    {:ok, %{key => content}}
-  end
-
-  def handle_event(
-        :end_document,
-        _,
-        {{key, attributes, value}, %{ignore_attribute: {false, _attribute_prefix}}}
-      ) do
-    content =
-      format_key_value_pairs(attributes) |> Map.put(@key_content, format_key_value_pairs(value))
-    {:ok, %{key => content}}
+  def handle_event(:end_document, _, {stack, ignore_attribute}) do
+    result = handle_end_document(stack, simplify_ignore_attribute_opt(ignore_attribute))
+    {:ok, result}
   end
 
   defp format_key_value_pairs(items) when is_list(items) do
@@ -237,15 +58,15 @@ defmodule SAXMap.Handler do
   end
 
   defp list_to_map([item | rest], prepared) when is_map(item) do
-    [{key, value}] = Map.to_list(item)
-    current_value = Map.get(prepared, key)
-    prepared = put_or_concat_to_map(current_value, prepared, key, value)
+    {key, value} = item |> Map.to_list() |> hd()
+    existed_value = Map.get(prepared, key)
+    prepared = put_or_concat_to_map(existed_value, prepared, key, value)
     list_to_map(rest, prepared)
   end
 
   defp list_to_map([{key, value} | rest], prepared) do
-    current_value = Map.get(prepared, key)
-    prepared = put_or_concat_to_map(current_value, prepared, key, value)
+    existed_value = Map.get(prepared, key)
+    prepared = put_or_concat_to_map(existed_value, prepared, key, value)
     list_to_map(rest, prepared)
   end
 
@@ -261,34 +82,21 @@ defmodule SAXMap.Handler do
     Map.put(map, key, [value, current_value])
   end
 
-  defp ignore_or_extract_characters(chars, state) do
+  defp ignore_or_extract_characters(chars, stack, value) do
     if String.trim(chars) == "" do
       # ignore
-      {:ok, state}
+      stack
     else
-      extract_characters(chars, state)
+      extract_characters_into_tag(chars, stack, value)
     end
   end
 
-  defp extract_characters(chars, {stack, %{ignore_attribute: true} = options}) do
-    [{tag_name, content} | stack] = stack
-    current = {tag_name, append_characters_text_content(content, chars)}
-    {:ok, {[current | stack], options}}
+  defp extract_characters_into_tag(chars, [{tag_name, content} | rest], true) do
+    [{tag_name, append_characters_text_content(content, chars)} | rest]
   end
 
-  defp extract_characters(chars, {stack, %{ignore_attribute: false} = options}) do
-    [{tag_name, attributes, content} | stack] = stack
-    current = {tag_name, attributes, append_characters_text_content(content, chars)}
-    {:ok, {[current | stack], options}}
-  end
-
-  defp extract_characters(
-         chars,
-         {stack, %{ignore_attribute: {false, _attribute_prefix}} = options}
-       ) do
-    [{tag_name, attributes, content} | stack] = stack
-    current = {tag_name, attributes, append_characters_text_content(content, chars)}
-    {:ok, {[current | stack], options}}
+  defp extract_characters_into_tag(chars, [{tag_name, attributes, content} | rest], false) do
+    [{tag_name, attributes, append_characters_text_content(content, chars)} | rest]
   end
 
   defp prepare_stack_text_when_start_element([{key, value} | rest]) when is_bitstring(value) do
@@ -308,5 +116,87 @@ defmodule SAXMap.Handler do
         Enum.reverse([%{@key_text_content => [chars]} | items])
     end
   end
+
+  defp handle_start_element({tag_name, _attributes}, stack, true) do
+    stack = prepare_stack_text_when_start_element(stack)
+    [{tag_name, nil} | stack]
+  end
+  defp handle_start_element({tag_name, attributes}, stack, false) do
+    stack = prepare_stack_text_when_start_element(stack)
+    [{tag_name, attributes, nil} | stack]
+  end
+  defp handle_start_element({tag_name, attributes}, stack, {false, attribute_prefix}) do
+    stack = prepare_stack_text_when_start_element(stack)
+    attributes =
+      Enum.map(attributes, fn {key, value} ->
+        {attribute_prefix <> key, value}
+      end)
+    [{tag_name, attributes, nil} | stack]
+  end
+
+  defp handle_cdata(cdata, [{tag_name, _} | rest]) do
+    [{tag_name, cdata} | rest]
+  end
+  defp handle_cdata(cdata, [{tag_name, attributes, _} | rest]) do
+    [{tag_name, attributes, cdata} | rest]
+  end
+
+  defp handle_characters("\r" <> _ = data, stack, value) do
+    ignore_or_extract_characters(data, stack, value)
+  end
+  defp handle_characters("\n" <> _ = data, stack, value) do
+    ignore_or_extract_characters(data, stack, value)
+  end
+  defp handle_characters(data, [{_not_end_tag, prepared}] = stack, value) when prepared != nil do
+    ignore_or_extract_characters(data, stack, value)
+  end
+  defp handle_characters(data, stack, value) do
+    extract_characters_into_tag(data, stack, value)
+  end
+
+  defp handle_end_element([{tag_name, content}], true) do
+    {tag_name, content}
+  end
+  defp handle_end_element([{tag_name, attributes, content}], false) do
+    {tag_name, attributes, content}
+  end
+  defp handle_end_element([{tag_name, content} | [{parenet_tag_name, nil} | rest]], true) do
+    current = {tag_name, format_key_value_pairs(content)}
+    [{parenet_tag_name, [current]} | rest]
+  end
+  defp handle_end_element([{tag_name, attributes, content} | [{parent_tag_name, parent_attributes, nil} | rest]], false) do
+    formated_content = format_key_value_pairs(content)
+    current = %{tag_name => format_key_value_pairs([{@key_content, formated_content} | attributes])}
+    [{parent_tag_name, parent_attributes, [current]} | rest]
+  end
+  defp handle_end_element([{tag_name, content} | [{parent_tag_name, parent_content} | rest]], true) do
+    current = {tag_name, format_key_value_pairs(content)}
+    [{parent_tag_name, [current | parent_content]} | rest]
+  end
+  defp handle_end_element([{tag_name, attributes, content} | [{parent_tag_name, parent_attributes, parent_content} | rest]], false) do
+    formated_content = format_key_value_pairs(content)
+    current = %{tag_name => format_key_value_pairs([{@key_content, formated_content} | attributes])}
+    [{parent_tag_name, parent_attributes, [current | parent_content]} | rest]
+  end
+
+  defp handle_end_document({key, nil}, true) do
+    %{key => %{}}
+  end
+  defp handle_end_document({key, value}, true) do
+    %{key => format_key_value_pairs(value)}
+  end
+  defp handle_end_document({key, attributes, nil}, false) do
+    %{key => format_key_value_pairs(attributes)}
+  end
+  defp handle_end_document({key, attributes, value}, false) do
+    value =
+      format_key_value_pairs(attributes) |> Map.put(@key_content, format_key_value_pairs(value))
+    %{key => value}
+  end
+
+  @compile {:inline, simplify_ignore_attribute_opt: 1}
+  defp simplify_ignore_attribute_opt(true), do: true
+  defp simplify_ignore_attribute_opt(false), do: false
+  defp simplify_ignore_attribute_opt({false, _}), do: false
 
 end
